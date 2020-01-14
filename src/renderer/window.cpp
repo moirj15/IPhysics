8edacb313829b9c451a4363717f98be99a7ae1e6
moirj15 <moirj15@gmail.com>
2020-01-14 10:33:13 -0500

    Rewrote the Window struct so it uses glfw instead of the Windows API.

--- src/renderer/window.cpp
+++ src/renderer/window.cpp
@@ -1,33 +1,18 @@
 #include "window.h"
 
-void Window::Init(MessageHandler messageHandler, HINSTANCE instanceHandle, int showCmd)
-{
-  WNDCLASS wc = {};
-  wc.style = CS_HREDRAW | CS_VREDRAW;
-  wc.lpfnWndProc = messageHandler;
-  wc.hInstance = instanceHandle;
-  wc.hIcon = LoadIcon(0, IDI_APPLICATION);
-  wc.hCursor = LoadCursor(0, IDC_ARROW);
-  wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-  wc.lpszClassName = m_title;
+#include <GLFW/glfw3.h>
+#include <assert.h>
 
-  if (!RegisterClass(&wc))
-  {
-    MessageBox(0, "RegisterClass Failed", 0, 0);
-    return;
-  }
-  m_windowHandle = CreateWindow(
-      m_title, m_title, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, m_width, m_height, 0, 0,
-      instanceHandle, 0);
-  if (!m_windowHandle)
-  {
-    MessageBox(0, "CreateWindow Failed", 0, 0);
-    return;
-  }
-  ShowWindow(m_windowHandle, true);
-  UpdateWindow(m_windowHandle);
-}
-
-void Window::Destroy()
+Window::Window(s32 width, s32 height, const char *title) :
+    mWidth(width), mHeight(height), mGLWindow(nullptr)
 {
+  mGLWindow = glfwCreateWindow(mWidth, mHeight, title, nullptr, nullptr);
+  if (!mGLWindow)
+  {
+    printf("FAILED TO CREATE GLFW WINDOW\n");
+    glfwTerminate();
+    assert(0);
+  }
+  glfwMakeContextCurrent(mGLWindow);
+  glViewport(0, 0, mWidth, mHeight);
 }
--- src/renderer/window.h
+++ src/renderer/window.h
@@ -1,21 +1,13 @@
 #pragma once
 #include "../common.h"
 
-#include <windows.h>
+struct GLFWwindow;
 
 struct Window
 {
-  s32 m_width;
-  s32 m_height;
-  const char *m_title;
-  HWND m_windowHandle;
+  s32 mWidth;
+  s32 mHeight;
+  GLFWwindow *mGLWindow;
 
-  using MessageHandler = LRESULT (*)(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-  Window(s32 width, s32 height, const char *title) :
-      m_width(width), m_height(height), m_title(title), m_windowHandle(0)
-  {
-  }
-
-  void Init(MessageHandler messageHandler, HINSTANCE instanceHandle, int showCmd);
-  void Destroy();
+  Window(s32 width, s32 height, const char *title);
 };
