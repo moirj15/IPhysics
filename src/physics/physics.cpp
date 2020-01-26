9d18acea9981cc21833cec721635589214d63675
moirj15 <moirj15@gmail.com>
2020-01-26 15:04:33 -0500

    Working on the physics engine using reactphysics3d

---
+++ src/physics/physics.cpp
@@ -0,0 +1,20 @@
+#include "physics.h"
+
+namespace pye
+{
+
+void PhysicsEngine::Update(f32 t)
+{
+  mObjectWorldCallback->Clear();
+  mObjectWorld->update(t);
+  mObjectWorld->testCollision(mObjectWorldCallback.get());
+  mCollisionOccuring = !mObjectWorldCallback->mCallbackInfo.empty();
+  if (mCollisionOccuring)
+  {
+    mVoxelWorld->update(t);
+    mVoxelWorld->testCollision(mVoxelWorldCallback.get());
+    // TODO: prevent voxels from drifting apart
+  }
+}
+
+} // namespace pye
---
+++ src/physics/physics.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "../common.h"
+
+#include <memory>
+#include <reactphysics3d.h>
+#include <vector>
+
+namespace pye
+{
+
+struct CollisionCallback final : public rp3d::CollisionCallback
+{
+  std::vector<rp3d::CollisionCallback::CollisionCallbackInfo> mCallbackInfo;
+  inline void notifyContact(const CollisionCallbackInfo &collisionCallbackInfo) override
+  {
+    mCallbackInfo.push_back(collisionCallbackInfo);
+  }
+
+  inline void Clear()
+  {
+    mCallbackInfo.clear();
+  }
+};
+
+class PhysicsEngine
+{
+  std::unique_ptr<rp3d::DynamicsWorld> mObjectWorld;
+  std::unique_ptr<rp3d::DynamicsWorld> mVoxelWorld;
+  std::unique_ptr<CollisionCallback> mObjectWorldCallback;
+  std::unique_ptr<CollisionCallback> mVoxelWorldCallback;
+
+  bool mCollisionOccuring;
+
+public:
+  PhysicsEngine() :
+      mObjectWorld(new rp3d::DynamicsWorld(rp3d::Vector3(0.0f, 0.0f, 0.0f))),
+      mVoxelWorld(new rp3d::DynamicsWorld(rp3d::Vector3(0.0f, 0.0f, 0.0f))),
+      mObjectWorldCallback(new CollisionCallback), mVoxelWorldCallback(new CollisionCallback),
+      mCollisionOccuring(false)
+  {
+  }
+
+  void Update(f32 t);
+
+  inline bool CollisionOccuring() const
+  {
+    return mCollisionOccuring;
+  }
+};
+
+} // namespace pye
